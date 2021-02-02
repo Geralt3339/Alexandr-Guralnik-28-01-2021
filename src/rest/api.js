357de63afcd90ee22b65c79b1c270ac4770e19d3
Alexandr G.S <4853941@gmail.com>
2021-02-03 00:53:05 +0200

    Final fixes

--- src/rest/api.js
+++ src/rest/api.js
@@ -3,5 +3,4 @@ export const Api = async (url, options = {}) => {
   return request
 }
 
-export const apikey = '3GMrbJnT3DWtScXewJtEuJ3Vm4HvkOeP'
-// export const apikey = '5B2X0vtpOp84BGI5GxudvGAcdx3UXs0U'
+export const apikey = '5B2X0vtpOp84BGI5GxudvGAcdx3UXs0U'
